bcd8ea7ee2d125932f59a1e913ac05e7bc7b98ab
srinadh1304 <srinadh1304@gmail.com>
2021-09-22 20:49:27 +0530

    MODIFIED: test cases for validating fare for multiple rides

--- lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -15,4 +15,21 @@ public class InvoiceGeneratorTest {
 		Assert.assertEquals(25, fare, 0.0);
 		
 	}
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMinimumfare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance =0.1;
+		int time = 1;
+		double fare  = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5,fare,0.0);
+	}
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalfare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(10, 3),
+						 new Ride(20, 6)
+					   	};
+		double fare  = invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(309,fare,0.0);
+	}
 }
