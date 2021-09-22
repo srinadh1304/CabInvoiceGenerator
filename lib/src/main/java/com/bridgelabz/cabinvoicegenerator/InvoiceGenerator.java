23f96638778d4048423bfeed36b2cd10571f3ed1
srinadh1304 <srinadh1304@gmail.com>
2021-09-22 21:30:26 +0530

    MODIFIED: method to get summary by user id

--- lib/src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.ArrayList;
+
 public class InvoiceGenerator {
 	private static final double MIN_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1; 
 	private static final int MINIMUM_COST = 5;
+	private static InvoiceService invoiceServiceObject = new InvoiceService();
 	
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MIN_COST_PER_KILOMETER + time * COST_PER_TIME;
@@ -22,4 +25,15 @@ public class InvoiceGenerator {
 		InvoiceSummary summary = new InvoiceSummary(numberOfRides,totalFare);
 		return summary;
 	}
+	public void addUserRide(String userId, Ride rides) {
+		invoiceServiceObject.addUserRide(userId, rides);
+	}
+	
+	public InvoiceSummary getSummaryByUserId(String userId) {
+		ArrayList<Ride> rides =  invoiceServiceObject.getUserRideList(userId);
+		Ride[] ridesArray = new Ride[rides.size()];
+		ridesArray = rides.toArray(ridesArray);
+		return this.getSummary(ridesArray);
+	}
+
 }
